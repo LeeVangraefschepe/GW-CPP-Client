512c6bce6091474f94502fa95d23a88110ec2249
LEEVGS <lee.vgs@hotmail.com>
2023-12-29 14:21:03 +0100

    Added support for release
    Changed chunk data with packet

--- Benchmarker/main.cpp
+++ Benchmarker/main.cpp
@@ -8,8 +8,8 @@ int main()
 {
 	unag::benchmark::Json benchmark{};
 
-	ChunkData data{};
-	data.set_packet_id(123);  // Assuming 123 is your desired packet ID
+	packet::ChunkData data{};
+	data.set_packetid(123);  // Assuming 123 is your desired packet ID
 	data.mutable_position()->set_x(456);
 	data.mutable_position()->set_y(789);
 	data.set_biome(5);
@@ -24,10 +24,10 @@ int main()
 
 	for (const auto& rowVector : value)
 	{
-		ChunkData::ChunkRow* row = data.add_chunk();
+		packet::ChunkData::ChunkRow* row = data.add_chunk();
 		for (const auto& sliceVector : rowVector)
 		{
-			ChunkData::ChunkSlice* slice = row->add_slices();
+			packet::ChunkData::ChunkSlice* slice = row->add_slices();
 			// Append the entire row at once using add_values
 			slice->mutable_values()->Add(sliceVector.begin(), sliceVector.end());
 		}
