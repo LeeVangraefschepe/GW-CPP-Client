ff0785de3c48cf691e4735155e78027e4163ebb9
LEEVGS <lee.vgs@hotmail.com>
2023-12-29 02:23:58 +0100

    Changed project to 32 bit and added protobuf

--- Benchmarker/main.cpp
+++ Benchmarker/main.cpp
@@ -2,10 +2,22 @@
 #include <string>
 #include "Benchmarks/Json.h"
 #include "Benchmarks/Xml.h"
+#include "Packets/Proto/Phone.pb.h"
 
 int main()
 {
 	unag::benchmark::Json benchmark{};
+
+	tutorial::Person test{};
+	test.set_number("Hello fking world");
+	test.set_id(69);
+
+	const std::string data{ test.SerializeAsString() };
+
+	tutorial::Person test2{};
+	test2.ParseFromString(data);
+	std::cout << test2.id() << ": " << test.number() << std::endl;
+
 
 	std::string line{};
 	while (true)
